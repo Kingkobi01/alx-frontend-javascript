c1a8aa9f69a013fcf265056d9e352ae63b503e48
Kwabena Boakye <kwaboakye93@gmail.com>
2023-11-30 13:58:06 +0000

    forEach

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,21 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((values) => (values));
+    .then((values) => {
+      const resultArray = [];
+      values.forEach((val) => {
+        if (val.status === 'fulfilled') {
+          resultArray.push({
+            status: val.status,
+            value: val.value,
+          });
+        } else {
+          resultArray.push({
+            status: val.status,
+            value: `Error: ${val.reason.message}`,
+          });
+        }
+      });
+      return resultArray;
+    });
 }
