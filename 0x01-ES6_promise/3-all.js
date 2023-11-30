af0f7f229e2e1ddffbf308c71abd58c052f776fd
Kwabena Boakye <kwaboakye93@gmail.com>
2023-11-30 13:40:21 +0000

    Add a return

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { createUser, uploadPhoto } from './utils';
 export default function handleProfileSignup() {
   const profilePhoto = uploadPhoto();
   const userProfile = createUser();
-  Promise.all([profilePhoto, userProfile])
+  return Promise.all([profilePhoto, userProfile])
     .then((value) => {
       const { body } = value[0];
       const { firstName, lastName } = value[1];
