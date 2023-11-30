57de0565f68612ac26fad3fd97647dd9cf841d74
Kwabena Boakye <kwaboakye93@gmail.com>
2023-11-30 18:36:16 +0000

    Hoisting

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -1,4 +1,3 @@
-// 9-hoisting.js
 export class HolbertonClass {
   constructor(year, location) {
     this._year = year;
@@ -13,7 +12,6 @@ export class HolbertonClass {
     return this._location;
   }
 }
-
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
@@ -30,14 +28,13 @@ export class StudentHolberton extends HolbertonClass {
   }
 
   get holbertonClass() {
-    return this.holbertonClass;
+    return this._holbertonClass;
   }
 
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
-
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
